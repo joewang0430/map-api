d38a2df669005f7544fb2216c9aaf032e8d6c47a
alex hu <alexhkq@gmail.com>
2025-01-10 18:11:48 -0500

    damn

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(void){
 
     cout << "This is a test\n" << endl;
     cout << "this is a lined added by Alex\n" << endl;
-    cout << "this is a lined added by Joe\n" << endl;
 
-   return 0;
+  return 0;
 }
